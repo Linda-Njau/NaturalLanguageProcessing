4364c421565e437bf8e15ad47bccce1d4c2f42b4
Linda-Njau <lindanjau21@gmail.com>
2025-05-19 16:46:50 +0300

    [PyTorch] Add dataset loading function with n-gram vocabulary builder for AG_NEWS

---
+++ Embeddings/torchnlp.py
@@ -0,0 +1,24 @@
+import builtins
+import torch
+import torchtext
+import collections
+import os
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+vocab = None
+tokenizer = torchtext.data.utils.get_tokenizer('basic_english')
+
+def load_dataset(ngrams=1,min_freq=1):
+    global vocab, tokenizer
+    print("Loading dataset...")
+    train_dataset, test_datset = torchtext.datasets.AG_NEWS(root='./data')
+    train_dataset = list(train_dataset)
+    test_datset = list(test_datset)
+    classes = ['world', 'sports', 'Business', 'Sci/Tech']
+    print('Building Vocab...')
+    counter = collections.Counter()
+    for (label, line) in train_dataset:
+        counter.update(torchtext.data.utils.ngrams_iterator(tokenizer(line),ngrams=ngrams))
+    vocab = torchtext.vocab.vocab(counter, min_freq=min_freq)
+    return train_dataset, test_datset, classes, vocab
